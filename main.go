515595396ee26eac467d5776dfb7d41b69f80af3
joweich <jonas.weich@outlook.de>
2021-01-21 10:56:04 +0100

    added json api to render multiple images per execution

--- main.go
+++ main.go
@@ -1,24 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"image"
 	"image/color"
 	"image/png"
+	"io/ioutil"
 	"log"
 	"math/cmplx"
 	"os"
 	"runtime"
 	"runtime/pprof"
+	"strconv"
 	"time"
 )
 
 // Configuration
 const (
-	xCenter = -0.75
-	yCenter =  0
-	zoom = 1
-
 	// Quality
 	imgWidth     = 1024
 	imgHeight    = 1024
@@ -34,30 +33,52 @@ const (
 	ratio = float64(imgWidth) / float64(imgHeight)
 )
 
-func main() {
-	log.Println("Allocating image...")
-	img := image.NewRGBA(image.Rect(0, 0, imgWidth, imgHeight))
-
-	log.Println("Rendering...")
-	start := time.Now()
-	render(img)
-	end := time.Now()
-
-	log.Println("Done rendering in", end.Sub(start))
-
-	log.Println("Encoding image...")
-	f, err := os.Create("result.png")
-	if err != nil {
-		panic(err)
-	}
-	err = png.Encode(f, img)
-	if err != nil {
-		panic(err)
-	}
-	log.Println("Done!")
+type location struct{
+	XCenter float64
+	YCenter float64
+	Zoom   float64
 }
 
-func render(img *image.RGBA) {
+type locationsFile struct {
+	Locations []location
+}
+
+func main() {
+	// locations from http://www.cuug.ab.ca/dewara/mandelbrot/images.html
+	log.Println("Reading location data...")
+	file, err := ioutil.ReadFile("locations.json")
+    if err != nil {
+      panic(err)
+	}
+
+	locs := locationsFile{}
+	_ = json.Unmarshal([]byte(file), &locs)
+	log.Printf("Found %v locations.", len(locs.Locations))
+	
+	start := time.Now()
+
+    for index, loc := range locs.Locations { 
+
+		log.Println("Allocating and rendering image ", index+1)
+		img := image.NewRGBA(image.Rect(0, 0, imgWidth, imgHeight))
+		render(img, loc)
+
+		log.Println("Encoding image ", index+1)
+		f, err := os.Create(strconv.FormatFloat(loc.Zoom, 'f', -1, 64) + "x_" + strconv.Itoa(index) + ".png")
+		if err != nil {
+			panic(err)
+		}
+		err = png.Encode(f, img)
+		if err != nil {
+			panic(err)
+		}
+	}
+	
+	end := time.Now()
+	log.Println("Done in", end.Sub(start))
+}
+
+func render(img *image.RGBA, loc location) {
 	if profileCpu {
 		f, err := os.Create("profile.prof")
 		if err != nil {
@@ -75,8 +96,8 @@ func render(img *image.RGBA) {
 				for x := 0; x < imgWidth; x++ {
 					var r, g, b int
 					for i := 0; i < samples; i++ {
-						nx := 3*(1/zoom)*ratio*((float64(x)+RandFloat64())/float64(imgWidth)-0.5) + xCenter
-						ny := 3*(1/zoom)*((float64(y)+RandFloat64())/float64(imgHeight)-0.5) - yCenter
+						nx := 3*(1/loc.Zoom)*ratio*((float64(x)+RandFloat64())/float64(imgWidth)-0.5) + loc.XCenter
+						ny := 3*(1/loc.Zoom)*((float64(y)+RandFloat64())/float64(imgHeight)-0.5) - loc.YCenter
 						c := paint(mandelbrotIterComplex(nx, ny, maxIter))
 						if linearMixing {
 							r += int(RGBToLinear(c.R))

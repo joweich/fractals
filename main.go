e9b9091e086d24b1074ad0bee5f432801b3b53ca
Jonas Weich <jns.wch@gmail.com>
2023-05-06 22:50:49 +0200

    Refactored config into command line arguments

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"image"
 	"image/color"
@@ -14,23 +15,17 @@ import (
 	"strconv"
 )
 
-// Configuration
-const (
-	// Quality
-	imgWidth       = 1024
-	imgHeight      = 1024
-	maxIter        = 1500
-	samples        = 50
-	hueOffset      = 0.0 // hsl color model; float in range [0,1)
-	linearMixing   = true
-	insideSetBlack = true
-)
-
-const (
-	ratio = float64(imgWidth) / float64(imgHeight)
-)
-
 func main() {
+	imgWidthPtr := flag.Int("width", 1920, "The width of the image in pixels.")
+	imgHeightPtr := flag.Int("height", 1024, "The height of the image in pixels.")
+	samplesPtr := flag.Int("samples", 50, "The number of samples")
+	maxIterPtr := flag.Int("iter", 500, "The max. number of iterations.")
+	hueOffsetPtr := flag.Float64("hue", 0.0, "The hsl hue offset in the range [0, 1)")
+	mixingPtr := flag.Bool("mixing", true, "Use linear color mixing.")
+	insideBlackPtr := flag.Bool("black", true, "Paint area inside in black.")
+
+	flag.Parse()
+
 	log.Println("Reading location data...")
 	file, err := os.ReadFile("locations.json")
 	if err != nil {
@@ -56,11 +51,11 @@ func main() {
 	for index, loc := range locs.Locations {
 
 		log.Println("Allocating and rendering image ", index+1)
-		img := image.NewRGBA(image.Rect(0, 0, imgWidth, imgHeight))
-		render(img, loc)
+		img := image.NewRGBA(image.Rect(0, 0, *imgWidthPtr, *imgHeightPtr))
+		render(img, loc, *samplesPtr, *maxIterPtr, *hueOffsetPtr, *mixingPtr, *insideBlackPtr)
 
 		log.Println("Encoding image ", index+1)
-		filename := "results/zoom" + strconv.FormatFloat(loc.Zoom, 'e', -1, 64) + "-iter" + strconv.Itoa(maxIter) + "-index" + strconv.Itoa(index+1)
+		filename := "results/zoom" + strconv.FormatFloat(loc.Zoom, 'e', -1, 64) + "-iter" + strconv.Itoa(*maxIterPtr) + "-index" + strconv.Itoa(index+1)
 		f, err := os.Create(filename + ".png")
 		if err != nil {
 			panic(err)
@@ -72,7 +67,11 @@ func main() {
 	}
 }
 
-func render(img *image.RGBA, loc Location) {
+func render(img *image.RGBA, loc Location, samples int, maxIter int, hueOffset float64, linearMixing bool, insideBlack bool) {
+	imgWidth := img.Rect.Max.X
+	imgHeight := img.Rect.Max.Y
+	ratio := float64(imgWidth) / float64(imgHeight)
+
 	jobs := make(chan int)
 
 	for i := 0; i < runtime.NumCPU(); i++ {
@@ -83,7 +82,7 @@ func render(img *image.RGBA, loc Location) {
 					for i := 0; i < samples; i++ {
 						nx := 3*(1/loc.Zoom)*ratio*((float64(x)+RandFloat64())/float64(imgWidth)-0.5) + loc.XCenter
 						ny := 3*(1/loc.Zoom)*((float64(y)+RandFloat64())/float64(imgHeight)-0.5) - loc.YCenter
-						c := paint(mandelbrotIterComplex(nx, ny, maxIter))
+						c := paint(mandelbrotIterComplex(nx, ny, maxIter, hueOffset, insideBlack))
 						if linearMixing {
 							r += int(RGBToLinear(c.R))
 							g += int(RGBToLinear(c.G))
@@ -117,31 +116,31 @@ func render(img *image.RGBA, loc Location) {
 	fmt.Printf("\r%d/%[1]d (100%%)\n", imgHeight)
 }
 
-func paint(magnitude float64, n int) color.RGBA {
+func paint(magnitude float64, maxIter int, hueOffset float64, insideBlack bool) color.RGBA {
 	if magnitude > 2 {
 		// adapted http://linas.org/art-gallery/escape/escape.html
 		nu := math.Log(math.Log(magnitude)) / math.Log(2)
-		hue := (float64(n)+1-nu)/float64(maxIter) + hueOffset
+		hue := (float64(maxIter)+1-nu)/float64(maxIter) + hueOffset
 		return hslToRGB(hue, 1, 0.5)
-	} else if insideSetBlack {
+	} else if insideBlack {
 		return color.RGBA{R: 0, G: 0, B: 0, A: 255}
 	} else {
 		return color.RGBA{R: 255, G: 255, B: 255, A: 255}
 	}
 }
 
-func mandelbrotIterComplex(px, py float64, maxIter int) (float64, int) {
+func mandelbrotIterComplex(px, py float64, maxIter int, hueOffset float64, insideBlack bool) (float64, int, float64, bool) {
 	var current complex128
 	pxpy := complex(px, py)
 
 	for i := 0; i < maxIter; i++ {
 		magnitude := cmplx.Abs(current)
 		if magnitude > 2 {
-			return magnitude, i
+			return magnitude, i, float64(hueOffset), insideBlack
 		}
 		current = current*current + pxpy
 	}
 
 	magnitude := cmplx.Abs(current)
-	return magnitude, maxIter
+	return magnitude, maxIter, hueOffset, insideBlack
 }
